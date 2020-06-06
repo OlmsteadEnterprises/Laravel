07118cce00c34a428c2d9d3c0176372c7a2e20b3
OlmsteadEnterprises <tolmstead7@gmail.com>
2020-06-06 12:40:13 -0600

    Finished course 14.

--- laravel-tutorial/routes/web.php
+++ laravel-tutorial/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return 'Hello World!!!';
+});
+Route::get('/user/{id}', function ($id) {
+    return 'Your user id is ' . $id;
+});
