90129871c2adeeeb7e0d60a13ca8a05fe0c13e57
OlmsteadEnterprises <tolmstead7@gmail.com>
2020-06-06 12:49:09 -0600

    Finished episode 15 Controllers.

--- laravel-tutorial/routes/web.php
+++ laravel-tutorial/routes/web.php
@@ -12,10 +12,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'StudentController@index');
+Route::get('/test', 'TestController@index');
 Route::get('/hello', function () {
     return 'Hello World!!!';
 });
